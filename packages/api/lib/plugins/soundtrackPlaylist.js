f44beaa1e3062766e2ac713e5ca788d19fe81965
Rickard Laurin <rickard.laurin@gmail.com>
2020-03-05 08:51:20 +0100

    feat(api): add spotify album id lookup

--- packages/api/lib/plugins/soundtrackPlaylist.js
+++ packages/api/lib/plugins/soundtrackPlaylist.js
@@ -2,12 +2,69 @@ const { makeExtendSchemaPlugin, gql } = require('graphile-utils')
 const axios = require('axios')
 const cheerio = require('cheerio')
 
+const getSpotifyId = async title => {
+  const {
+    data: { tracks: [spotify] = [] },
+  } = await axios.get(
+    `https://wejay.willcodefor.beer/search?q=album:${encodeURI(title)}`
+  )
+
+  return spotify ? spotify.albumId : null
+}
+
+const getIMDbId = async title => {
+  const { data: imdbData } = await axios.get(
+    `https://www.imdb.com/find?q=${encodeURI(title)}`
+  )
+  const $ = cheerio.load(imdbData)
+
+  const id = $('.result_text a')
+    .first()
+    .attr('href')
+    .match(/tt\d+/)
+
+  return id.length ? id[0] : null
+}
+
+const getReleaseDate = $ => {
+  return $('#titlebox p')
+    .filter(function() {
+      return $(this)
+        .text()
+        .includes('Release Date:')
+    })
+    .text()
+    .replace('Release Date: ', '')
+    .trim()
+}
+
+const getPlaylist = $ => {
+  return $('.cbox_table tbody tr:not(:last-child)')
+    .map(function(i) {
+      const title = $(this)
+        .find('td:nth-child(2)')
+        .contents()
+        .first()
+        .text()
+        .trim()
+      const playtime = $(this)
+        .find('td:nth-child(3)')
+        .text()
+        .trim()
+
+      return `${i + 1};${title};${playtime}`
+    })
+    .get()
+    .join('\n')
+}
+
 const SoundtrackPlaylistPlugin = makeExtendSchemaPlugin(() => {
   return {
     typeDefs: gql`
       type SoundtrackPlaylist {
         title: String!
         imdbId: String
+        spotifyId: String
         releaseDate: String!
         playlist: String!
       }
@@ -26,51 +83,17 @@ const SoundtrackPlaylistPlugin = makeExtendSchemaPlugin(() => {
           const $ = cheerio.load(data)
 
           const title = $('#titlebox h1').text()
-
-          const { data: imdbData } = await axios.get(
-            `https://www.imdb.com/find?q=${title}`
-          )
-          const imdb = cheerio.load(imdbData)
-
-          const imdbId = imdb('.result_text a')
-            .first()
-            .attr('href')
-
-            .match(/tt\d+/)
-
-          const releaseDate = $('#titlebox p')
-            .filter(function() {
-              return $(this)
-                .text()
-                .includes('Release Date:')
-            })
-            .text()
-            .replace('Release Date: ', '')
-            .trim()
-
-          const playlist = $('.cbox_table tbody tr:not(:last-child)')
-            .map(function(i) {
-              const title = $(this)
-                .find('td:nth-child(2)')
-                .contents()
-                .first()
-                .text()
-                .trim()
-              const playtime = $(this)
-                .find('td:nth-child(3)')
-                .text()
-                .trim()
-
-              return `${i + 1};${title};${playtime}`
-            })
-            .get()
-            .join('\n')
+          const spotifyId = await getSpotifyId(title)
+          const imdbId = await getIMDbId(title)
+          const releaseDate = await getReleaseDate($)
+          const playlist = await getPlaylist($)
 
           return {
-            title,
-            releaseDate,
-            imdbId: imdbId.length ? imdbId[0] : null,
+            imdbId,
             playlist,
+            releaseDate,
+            spotifyId,
+            title,
           }
         },
       },
